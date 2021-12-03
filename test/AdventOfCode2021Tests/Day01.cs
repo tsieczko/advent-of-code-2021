98fd2fd3f08e1c425f17d787af51c76fa3a7affe
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-02 23:51:24 -0800

    add test project

---
+++ test/AdventOfCode2021Tests/Day01.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode2021;
+
+namespace AdventOfCode2021Tests
+{
+		[TestClass]
+		public class Day01
+		{
+				[TestMethod]
+				public void TestSonarSweep1()
+				{
+					
+				}
+		}
+}
