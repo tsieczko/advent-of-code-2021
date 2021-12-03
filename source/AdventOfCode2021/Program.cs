10f684b5c00cb41a3b9cddf9443c81942f213b9b
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-02 23:20:28 -0800

    create project for each day

---
+++ source/AdventOfCode2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SonarSweep1.Run("../Day01/test_input.txt");
+        }
+    }
+}
--- source/Day01/SonarSweep/SonarSweep1.cs
+++ source/Day01/SonarSweep/SonarSweep1.cs
@@ -2,21 +2,19 @@ using System;
 using System.IO;
 using System.Linq;
 
-namespace advent_of_code_2021
+namespace AdventOfCode2021
 {
-	public class SonarSweep2
+	public class SonarSweep1
 	{
 		public static void Run(string path)
 		{
 			var foundDepthStrings = File.ReadAllLines(path);
-			var foundDepths = foundDepthStrings.Select(x => int.Parse(x)).ToList();
+			var foundDepths = foundDepthStrings.Select(x => int.Parse(x));
 
-			int previousDepth = -1;
 			int numIncreasedMeasurements = 0;
-			for (int i = 1; i < foundDepths.Count() - 1; i++)
+			int previousDepth = -1;
+			foreach (var depth in foundDepths)
 			{
-				var depth = foundDepths[i-1] + foundDepths[i] + foundDepths[i+1];
-
 				if (previousDepth == -1)
 				{
 					Console.WriteLine($"{depth} (N/A - no previous measurement)");
