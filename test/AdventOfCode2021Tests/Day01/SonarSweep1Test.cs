143d73297727f714ddc1604b9572d6a02548f29b
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-03 00:19:45 -0800

    Move file

---
+++ test/AdventOfCode2021Tests/Day01/SonarSweep1Test.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode2021;
+
+namespace AdventOfCode2021Tests
+{
+    [TestClass]
+    public class SonarSweep1Test
+    {
+        [TestMethod]
+        public void TestSonarSweep1()
+        {
+            
+        }
+    }
+}
