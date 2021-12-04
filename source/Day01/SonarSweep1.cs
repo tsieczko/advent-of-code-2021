c2d9dcf9075e5a97773380faef889d786683c22e
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-03 18:59:25 -0800

    Tests for SonarSweep1

--- source/Day01/SonarSweep1.cs
+++ source/Day01/SonarSweep1.cs
@@ -1,45 +1,19 @@
-using System;
-using System.IO;
-using System.Linq;
-
 namespace AdventOfCode2021
 {
-	public class SonarSweep1
+    public class SonarSweep1
 	{
-		public static void Run(string path)
+		public static int Run(int[] depths)
 		{
-			var foundDepthStrings = File.ReadAllLines(path);
-			var foundDepths = foundDepthStrings.Select(x => int.Parse(x));
-
 			int numIncreasedMeasurements = 0;
-			int previousDepth = -1;
-			foreach (var depth in foundDepths)
+			for (var i = 1; i < depths.Length; i++)
 			{
-				if (previousDepth == -1)
-				{
-					Console.WriteLine($"{depth} (N/A - no previous measurement)");
-					previousDepth = depth;
-					continue;
-				}
-
-				if (depth > previousDepth)
-				{
-					Console.WriteLine($"{depth} (increased)");
+				if (depths[i] > depths[i-1])
+                {
 					numIncreasedMeasurements++;
-				}
-				else if (depth < previousDepth)
-				{
-					Console.WriteLine($"{depth} (decreased)");
-				}
-				else
-				{
-					Console.WriteLine($"{depth} (no change)");
-				}
-
-				previousDepth = depth;
+                }
 			}
 
-			Console.WriteLine(numIncreasedMeasurements);
+			return numIncreasedMeasurements;
 		}
 	}
 }
