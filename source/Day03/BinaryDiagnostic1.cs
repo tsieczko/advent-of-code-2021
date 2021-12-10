cd58d368f6b8e824133634182c1ec5b9896e3411
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-09 21:36:12 -0800

    Finish BinaryDiagnostic1

---
+++ source/Day03/BinaryDiagnostic1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2021
+{
+    public static class BinaryDiagnostic1
+    {
+        public static int Run(string[] binaryNumbers)
+        {
+            var orderTo1Frequency = new Dictionary<int, int>();
+
+            foreach (var number in binaryNumbers)
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    var currentIndex = number.Length - 1 - i;
+
+                    if (!orderTo1Frequency.ContainsKey(currentIndex))
+                    {
+                        orderTo1Frequency[currentIndex] = 0;
+                    }
+
+                    if (number[i] == '1')
+                    {
+                        orderTo1Frequency[currentIndex] += 1;
+                    }
+                }
+            }
+
+            var gammaRate = GetGammaRate(orderTo1Frequency, binaryNumbers.Length);
+
+            var epsilonRate = gammaRate;
+            for (int i = 0; i < binaryNumbers[0].Length; i++)
+            {
+                epsilonRate ^= 1 << i;
+            }
+
+            var powerConsumption = gammaRate * epsilonRate;
+
+            return powerConsumption;
+        }
+
+        private static int GetGammaRate(IDictionary<int, int> orderTo1Frequency, int totalRows)
+        {
+            var result = 0;
+
+            foreach (var (order, frequency) in orderTo1Frequency)
+            {
+                if (frequency > (totalRows / 2))
+                {
+                    result |= 1 << order;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/Day03/BinaryDiagnostic1Test.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021Tests
+{
+    [TestClass]
+    public class BinaryDiagnostic1Test : TestBase
+    {
+        protected override string DayFolderName => "Day03";
+
+        [TestMethod]
+        public override void TestInput()
+        {
+            // read in test input
+            var binaryNumbers = File.ReadAllLines(TestInputPath);
+            var result = BinaryDiagnostic1.Run(binaryNumbers);
+
+            Assert.AreEqual(198, result);
+        }
+
+        [TestMethod]
+        public override void PuzzleInput()
+        {
+            // read in test input
+            var binaryNumbers = File.ReadAllLines(PuzzleInputPath);
+            var result = BinaryDiagnostic1.Run(binaryNumbers);
+
+            Assert.AreEqual(2954600, result);
+        }
+    }
+}
