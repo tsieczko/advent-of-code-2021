f7e65a321bcc2c3767cdcee7addac998f240464f
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-09 22:42:42 -0800

    Finish day3

---
+++ source/Day03/BinaryDiagnostic2.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2021
+{
+    public static class BinaryDiagnostic2
+    {
+        public static int Run(string[] binaryNumbers)
+        {
+            var O2Rating = GetRating(binaryNumbers, isOxygen: true).ToInt();
+            var CO2Rating = GetRating(binaryNumbers, isOxygen: false).ToInt();
+
+            return O2Rating * CO2Rating;
+        }
+
+        private static string GetRating(IList<string> binaryNumbers, bool isOxygen, int bitPosition = 0)
+        {
+            if (binaryNumbers.Count == 1)
+            {
+                return binaryNumbers[0];
+            }
+
+            var firstBit0 = new List<string>();
+            var firstBit1 = new List<string>();
+
+            foreach (var number in binaryNumbers)
+            {
+                if (number.IsBit0(bitPosition))
+                {
+                    firstBit0.Add(number);
+                }
+                else
+                {
+                    firstBit1.Add(number);
+                }
+            }
+
+            if (isOxygen)
+            {
+                if (firstBit1.Count >= firstBit0.Count)
+                {
+                    return GetRating(firstBit1, isOxygen, bitPosition + 1);
+                }
+                else
+                {
+                    return GetRating(firstBit0, isOxygen, bitPosition + 1);
+                }
+            }
+            else // isCO2
+            {
+                if (firstBit0.Count <= firstBit1.Count)
+                {
+                    return GetRating(firstBit0, isOxygen, bitPosition + 1);
+                }
+                else
+                {
+                    return GetRating(firstBit1, isOxygen, bitPosition + 1);
+                }
+            }
+        }
+
+        private static bool IsBit0(this string bitString, int bitPosition)
+        {
+            return bitString[bitPosition] == '0';
+        }
+
+        private static int ToInt(this string bitString)
+        {
+            int result = 0;
+
+            for (int i = 0; i < bitString.Length; i++)
+            {
+                var currentBit = bitString[i] == '1' ? 1 : 0;
+                var currentIndex = bitString.Length - 1 - i;
+                result |= currentBit << currentIndex;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/Day03/BinaryDiagnostic2Test.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace AdventOfCode2021Tests
+{
+    [TestClass]
+    public class BinaryDiagnostic2Test : TestBase
+    {
+        protected override string DayFolderName => "Day03";
+
+        [TestMethod]
+        public override void TestInput()
+        {
+            // read in test input
+            var binaryNumbers = File.ReadAllLines(TestInputPath);
+            var result = BinaryDiagnostic2.Run(binaryNumbers);
+
+            Assert.AreEqual(230, result);
+        }
+
+        [TestMethod]
+        public override void PuzzleInput()
+        {
+            // read in test input
+            var binaryNumbers = File.ReadAllLines(PuzzleInputPath);
+            var result = BinaryDiagnostic2.Run(binaryNumbers);
+
+            Assert.AreEqual(1662846, result);
+        }
+    }
+}
