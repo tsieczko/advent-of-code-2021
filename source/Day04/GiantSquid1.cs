bbed761eb63d31a1c33274f7a87110862b276124
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2021-12-22 22:07:45 -0800

    wip

---
+++ source/Day04/GiantSquid1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day04
+{
+    public static class GiantSquid1
+    {
+        public static int Run(string[] lines)
+        {
+            int[] drawnNumbers;
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                // handle drawn numbers
+                if (i == 0)
+                {
+                    drawnNumbers = HandleDrawnNumbers(lines[0]);
+                }
+
+                if (lines[i] != "\n")
+                {
+                    var boardLines = lines[i..(i + 4)];
+                    i += 5;
+                }
+            }
+            return 0;
+        }
+
+        public static int[] HandleDrawnNumbers(string commaSeparatedNumbersString)
+        {
+            var numberStrings = commaSeparatedNumbersString.Split(",");
+            var parsedNumbers = numberStrings.Select(numberString => int.Parse(numberString));
+
+            return parsedNumbers.ToArray(); ;
+        }
+    }
+}
---
+++ test/Day04/GiantSquid1Test.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode2021.Day04;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace AdventOfCode2021Tests.Day04
+{
+    [TestClass]
+    public class GiantSquid1Test : TestBase
+    {
+        protected override string DayFolderName => "Day04";
+
+        [TestMethod]
+        public override void PuzzleInput()
+        {
+            var lines = File.ReadAllLines(PuzzleInputPath);
+            var result = GiantSquid1.Run(lines);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public override void TestInput()
+        {
+            var lines = File.ReadAllLines(TestInputPath);
+            var result = GiantSquid1.Run(lines);
+
+            Assert.AreEqual(4512, result);
+        }
+    }
+}
