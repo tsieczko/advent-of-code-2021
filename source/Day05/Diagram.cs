1bc8768a7648734aa9e5624341671f5b057be8c8
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2022-01-15 23:08:25 -0800

    Complete Diagram

--- source/Day05/Diagram.cs
+++ source/Day05/Diagram.cs
@@ -1,30 +1,98 @@
+using System.Collections.Generic;
 using System.Text;
 
 namespace AdventOfCode2021.Day05
 {
 	public class Diagram
 	{
-		const int _diagramSize = 1000;
-		int[,] _diagram;
+		private readonly int _diagramSize;
+		private readonly int[,] _diagram;
+		private readonly List<Line> _lines = new();
 
-		public Diagram()
+		public Diagram(int size)
 		{
+			_diagramSize = size;
 			_diagram = new int[_diagramSize, _diagramSize];
+		}
+
+		public Diagram AddLine(Line line)
+		{
+			if (line.Point1.X == line.Point2.X || line.Point1.Y == line.Point2.Y)
+			{
+				_lines.Add(line);
+			}
+
+			return this;
+		}
+
+		public int[,] Draw()
+		{
+			foreach (var line in _lines)
+			{
+				MarkLine(line);
+			}
+
+			return _diagram;
 		}
 
 		public override string ToString()
 		{
 			var stringBuilder = new StringBuilder();
 
-			for (int i = 0; i < _diagramSize; i++)
+			for (int y = 0; y < _diagramSize; y++)
 			{
-				for (int j = 0; j < _diagramSize; j++)
+				for (int x = 0; x < _diagramSize; x++)
 				{
-					stringBuilder.Append($"{_diagram[i, j]:d4}").Append(' ');
+					stringBuilder.Append($"{_diagram[y, x],4}").Append(' ');
 				}
+				stringBuilder.AppendLine();
 			}
 
 			return stringBuilder.ToString();
+		}
+
+		private void MarkLine(Line line)
+		{
+			var current = line.Point1;
+			var end = line.Point2;
+
+			while (true)
+			{
+				MarkPoint(current);
+
+				if (current.Equals(end))
+				{
+					break;
+				}
+
+				current = MovePoint(current, line.GetDirection());
+			} 
+		}
+
+		private void MarkPoint(Point point)
+		{
+			_diagram[point.Y, point.X] += 1;
+		}
+
+		private static Point MovePoint(Point point, Direction direction)
+		{
+			switch (direction)
+			{
+				case Direction.North:
+					point.Y -= 1;
+					break;
+				case Direction.South:
+					point.Y += 1;
+					break;
+				case Direction.East:
+					point.X += 1;
+					break;
+				case Direction.West:
+					point.X -= 1;
+					break;
+			}
+
+			return point;
 		}
 	}
 }
---
+++ test/Day05/DiagramTest.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode2021.Day05;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2021Tests.Day05
+{
+	[TestClass]
+	public class DiagramTest
+	{
+		Diagram Diagram { get; set; }
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			Diagram = new Diagram(size: 5);
+		}
+
+		[TestMethod]
+		public void TestDiagramHorizontalLine()
+		{
+			var expected = new int[5, 5]
+			{
+				{ 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0 },
+				{ 1, 1, 1, 1, 1 },
+				{ 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0 }
+			};
+
+			var actual = Diagram.AddLine(new Line()
+			{
+				Point1 = new Point(0, 2),
+				Point2 = new Point(4, 2)
+			}).Draw();
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestDiagramVerticalLine()
+		{
+			var expected = new int[5, 5]
+			{
+				{ 0, 0, 1, 0, 0 },
+				{ 0, 0, 1, 0, 0 },
+				{ 0, 0, 1, 0, 0 },
+				{ 0, 0, 1, 0, 0 },
+				{ 0, 0, 1, 0, 0 }
+			};
+
+			var actual = Diagram.AddLine(new Line()
+			{
+				Point1 = new Point(2, 0),
+				Point2 = new Point(2, 4)
+			}).Draw();
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+	}
+}
