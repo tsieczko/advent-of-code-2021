f740cb36c8a86231a8349b0a0f5502950823c616
Tomasz Sieczko <sieczko.tomasz@gmail.com>
2022-05-19 23:20:22 -0700

    setup day 12

---
+++ source/Day12/PassagePathing2.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2021.Day12
+{
+	public static class PassagePathing2
+	{
+		public static int Run(string[] lines)
+		{
+			return -1;
+		}
+	}
+}
---
+++ test/Day12/PassagePathing2Test.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using AdventOfCode2021.Day12;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2021Tests.Day12
+{
+	[TestClass]
+	public class PassagePathing2Test : TestBase
+	{
+		protected override string DayFolderName => "Day12";
+
+		[TestMethod]
+		public override void PuzzleInput()
+		{
+			var lines = File.ReadAllLines(PuzzleInputPath);
+			var result = PassagePathing2.Run(lines);
+
+			Assert.AreEqual(348, result);
+		}
+
+		[TestMethod]
+		public override void TestInput()
+		{
+			var lines = File.ReadAllLines(TestInputPath);
+			var result = PassagePathing2.Run(lines);
+
+			Assert.AreEqual(195, result);
+		}
+	}
+}
